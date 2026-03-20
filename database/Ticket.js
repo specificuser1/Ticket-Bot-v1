c8e6a788e37b08b39e5dc029050f66d450201122
specificuser1 <motorolag14rx@gmail.com>
2026-03-21 01:14:11 +0500

    Delete database/Ticket.js

--- database/Ticket.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const TicketSchema = new mongoose.Schema({
-  user: String,
-  channel: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Ticket", TicketSchema);
